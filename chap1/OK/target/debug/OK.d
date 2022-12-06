c95f7043c729e9428cb1bd02a6ac53599f5b5eb8
Stephen Akuffo <sa.akuffo@gmail.com>
2022-12-05 23:29:03 -0500

    remove target

--- chap1/OK/target/debug/OK.d
+++
@@ -1,1 +0,0 @@
-C:\Users\saaku\the-lab\1-projects\rust-in-action\chap1\OK\target\debug\OK.exe: C:\Users\saaku\the-lab\1-projects\rust-in-action\chap1\OK\src\main.rs
--- chap1/OK/target/debug/deps/OK.d
+++
@@ -1,5 +0,0 @@
-C:\Users\saaku\the-lab\1-projects\rust-in-action\chap1\OK\target\debug\deps\ok.exe: src\main.rs
-
-C:\Users\saaku\the-lab\1-projects\rust-in-action\chap1\OK\target\debug\deps\ok.d: src\main.rs
-
-src\main.rs:
