6562323214185a6a3688640573e913cd61474609
Stephen Akuffo <sa.akuffo@gmail.com>
2022-12-05 23:27:36 -0500

    remove target

--- chap1/vars/target/debug/deps/vars.d
+++
@@ -1,5 +0,0 @@
-C:\Users\saaku\the-lab\1-projects\rust-in-action\chap1\vars\target\debug\deps\vars.exe: src\main.rs
-
-C:\Users\saaku\the-lab\1-projects\rust-in-action\chap1\vars\target\debug\deps\vars.d: src\main.rs
-
-src\main.rs:
--- chap1/vars/target/debug/vars.d
+++
@@ -1,1 +0,0 @@
-C:\Users\saaku\the-lab\1-projects\rust-in-action\chap1\vars\target\debug\vars.exe: C:\Users\saaku\the-lab\1-projects\rust-in-action\chap1\vars\src\main.rs
