a681d002a491af67724387da6d95669c766fd1f6
Stephen Akuffo <sa.akuffo@gmail.com>
2022-12-05 23:26:20 -0500

    win commit

---
+++ chap1/OK/target/debug/OK.d
@@ -0,0 +1,1 @@
+C:\Users\saaku\the-lab\1-projects\rust-in-action\chap1\OK\target\debug\OK.exe: C:\Users\saaku\the-lab\1-projects\rust-in-action\chap1\OK\src\main.rs
---
+++ chap1/vars/target/debug/vars.d
@@ -0,0 +1,1 @@
+C:\Users\saaku\the-lab\1-projects\rust-in-action\chap1\vars\target\debug\vars.exe: C:\Users\saaku\the-lab\1-projects\rust-in-action\chap1\vars\src\main.rs
